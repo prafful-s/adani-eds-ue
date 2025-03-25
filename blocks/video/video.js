a09c1f904de7980f04d4d60f7ebb46849d93b7f3
praffuls <praffuls@adobe.com>
2025-03-25 14:50:30 +0530

    Code updates for video block

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -2,8 +2,7 @@ export default function decorateBlock(block) {
   const url = 'https://www.adanienterprises.com/-/media/Project/Enterprises/Home/AEL_vid';
   const wrapper = document.createElement('div');
   wrapper.className = 'video-placeholder';
-  wrapper.append(
-    '<video preload="auto" loop="true" autoplay="true" muted=""><source src="' +
-    url + '"type="video/mp4"></video>');
+  wrapper.innerHTML = '<video preload="auto" loop="true" autoplay="true" muted=""><source src="' +
+    url + '"type="video/mp4"></video>';
   block.append(wrapper);
 }
