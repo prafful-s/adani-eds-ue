c7be6a9190859187c8f53d87898c72404492ba71
praffuls <praffuls@adobe.com>
2025-03-25 23:06:18 +0530

    Code changes for video component

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -117,7 +117,11 @@
 //   decorate(block);
 // }
 
-
-export default function decorate(block) {
-  console.log("video component called successfully");
+export default function decorateBlock(block) {
+  const url = 'https://www.adanienterprises.com/-/media/Project/Enterprises/Home/AEL_vid';
+  const wrapper = document.createElement('div');
+  wrapper.className = 'video-placeholder';
+  wrapper.innerHTML = '<video preload="auto" loop="true" autoplay="true" muted=""><source src="' +
+    url + '"type="video/mp4"></video>';
+  block.append(wrapper);
 }
