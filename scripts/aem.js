ccf85d5754f98aea074b8f64a25f1cdcae688d92
pankaj2186 <pansingh@adobe.com>
2025-03-26 11:28:01 +0530

    Update aem.js

--- scripts/aem.js
+++ scripts/aem.js
@@ -591,7 +591,13 @@ async function loadBlock(block) {
     block.dataset.blockStatus = 'loading';
     const { blockName } = block.dataset;
     try {
-      const cssLoaded = loadCSS(`${window.hlx.codeBasePath}/blocks/${blockName}/${blockName}.css`);
+      const cssLoaded;
+      if (blockName === "footer" && window.location.href.includes("/content")){
+         cssLoaded = loadCSS(`${window.hlx.codeBasePath}/blocks/${blockName}/${blockName}-ue.css`);
+      }else{
+        cssLoaded = loadCSS(`${window.hlx.codeBasePath}/blocks/${blockName}/${blockName}.css`);
+      }
+      
       const decorationComplete = new Promise((resolve) => {
         (async () => {
           try {
