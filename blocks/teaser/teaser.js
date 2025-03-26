3654a672353b525f0e1fe9930df404b1954a5d4d
praffuls <praffuls@adobe.com>
2025-03-26 13:21:35 +0530

    Code update for teaser component

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,19 +1,19 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+// import { createOptimizedPicture } from '../../scripts/aem.js';
 
-export default function decorate(block) {
-  console.log("teaser component called successfully");
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'teaser-image';
-      else div.className = 'teaser-body';
-    });
-    ul.append(li);
-  });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
-}
+// export default function decorate(block) {
+//   console.log("teaser component called successfully");
+//   /* change to ul, li */
+//   const ul = document.createElement('ul');
+//   [...block.children].forEach((row) => {
+//     const li = document.createElement('li');
+//     while (row.firstElementChild) li.append(row.firstElementChild);
+//     [...li.children].forEach((div) => {
+//       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'teaser-image';
+//       else div.className = 'teaser-body';
+//     });
+//     ul.append(li);
+//   });
+//   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+//   block.textContent = '';
+//   block.append(ul);
+// }
