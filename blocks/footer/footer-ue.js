2f0db4b0cacf3888d7f80b6026887f62ae20fba8
pankaj2186 <pansingh@adobe.com>
2025-03-26 11:49:27 +0530

    Update footer-ue.js

--- blocks/footer/footer-ue.js
+++ blocks/footer/footer-ue.js
@@ -390,3 +390,24 @@ document.addEventListener('DOMContentLoaded', () => {
   // Transform the footer
   transformToAdaniFooter();
 });
+
+/**
+ * loads and decorates the footer
+ * @param {Element} block The footer block element
+ */
+export default async function decorate(block) {
+  // load footer as fragment
+  const footerMeta = getMetadata('footer');
+  const pathSegments = window.location.pathname.split('/').filter(Boolean);
+  const parentPath = pathSegments.length > 1 ? `/${pathSegments.slice(0, -1).join('/')}/` : '/';
+  //const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  const footerPath = parentPath=='/' ? footerMeta ? new URL(footerMeta, window.location).pathname : '/footer' : footerMeta ? new URL(footerMeta, window.location).pathname : parentPath+'/footer';
+  const fragment = await loadFragment(footerPath);
+
+  // decorate footer DOM
+  block.textContent = '';
+  const footer = document.createElement('div');
+  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+
+  block.append(footer);
+}
