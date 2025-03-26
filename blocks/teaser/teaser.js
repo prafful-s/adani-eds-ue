29f984cade3a610c69693d91b47728353ad5b455
pankaj2186 <pansingh@adobe.com>
2025-03-26 13:05:27 +0530

    Update teaser.js

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -8,8 +8,8 @@ export default function decorate(block) {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'teaser-image';
+      else div.className = 'teaser-body';
     });
     ul.append(li);
   });
